619d61fb02905eec8fb185fd4e58f9b87fa9d550
SUDHANSHU DUBEY <manudubey249@gmail.com>
2025-09-10 10:06:25 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 const app = require(".");
 const connectDb = require("./config/db");
+const cors = require('cors'); 
+
+
+app.use(cors({
+  origin: 'https://e-commerce-frontend-pi-ochre.vercel.app/',
+  credentials: true
+}));
 
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, async () => {
-    await connectDb();
+  await connectDb();
   console.log(`Server is running on port ${PORT}`);
 });
