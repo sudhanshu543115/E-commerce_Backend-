9958f7df085bbb79f3bd0574dc4edd7b36827bbc
SUDHANSHU DUBEY <manudubey249@gmail.com>
2025-09-26 15:33:47 +0530

    Update HomeSectionCard.jsx

--- frontend/src/Customer/componants/HomeSectionCard/HomeSectionCard.jsx
+++ frontend/src/Customer/componants/HomeSectionCard/HomeSectionCard.jsx
@@ -4,7 +4,7 @@ const HomeSectionCard = ({ product, index }) => {
   const navigate = useNavigate();
 
   return (
-    <div onClick={() => {navigate(`/product/${product._id || product.id}`)}} className="pt-5 flex flex-col items-center justify-center bg-white shadow-lg rounded-lg overflow-hidden w-[15rem] mx-3 hover:shadow-xl transition-shadow duration-300 gap-6">
+    <div onClick={() => {navigate(`/product/${product.id}`)}} className="pt-5 flex flex-col items-center justify-center bg-white shadow-lg rounded-lg overflow-hidden w-[15rem] mx-3 hover:shadow-xl transition-shadow duration-300 gap-6">
       <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg overflow-hidden w-[15rem] mx-3 hover:shadow-xl transition-shadow duration-300">
         <div className="h-[13rem] w-[10rem]">
           <img
