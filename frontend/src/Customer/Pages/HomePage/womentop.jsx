a7f027573bf5e72a7a3fdbec0caf00426f75c770
SUDHANSHU DUBEY <manudubey249@gmail.com>
2025-09-26 14:46:28 +0530

    Update womentop.jsx

--- frontend/src/Customer/Pages/HomePage/womentop.jsx
+++ frontend/src/Customer/Pages/HomePage/womentop.jsx
@@ -11,7 +11,7 @@ const Homepage = () => {
     const fetchProducts = async () => {
       try {
         setLoading(true);
-        const response = await fetch('https://e-commerce-api-wvt2.onrender.com/api/products');
+        const response = await fetch('https://e-commerce-api-geon.onrender.com/api/products');
         
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -55,3 +55,4 @@ const Homepage = () => {
 }
 
 export default Homepage;
+
